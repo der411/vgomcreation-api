332a5d10133db1a494c6827fecc9d18ea6abf658
der411 <vaitilingomvincent@gmail.com>
2024-11-23 19:02:11 +0100

    cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,37 +1,38 @@
 module.exports = ({ env }) => [
-    'strapi::errors',
-    {
+  'strapi::errors',
+  {
       name: 'strapi::security',
       config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-            'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-            upgradeInsecureRequests: null,
+          contentSecurityPolicy: {
+              useDefaults: true,
+              directives: {
+                  'connect-src': ["'self'", 'https:'],
+                  'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+                  'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+                  upgradeInsecureRequests: null,
+              },
           },
-        },
       },
-    },
-    {
+  },
+  {
       name: 'strapi::cors',
       config: {
-        enabled: true,
-        origin: [
-          'http://localhost:3000', // Frontend local
-          'https://vgomcreation-fullstack.vercel.app', // Frontend déployé
-          'https://vgomcreation-api-production.up.railway.app', // Backend déployé
-        ],
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        headers: ['Content-Type', 'Authorization'],
+          enabled: true,
+          origin: [
+              'http://localhost:3000', // Frontend local
+              'https://vgomcreation-fullstack.vercel.app', // Frontend déployé
+              'https://vgomcreation-api-production.up.railway.app', // Backend déployé
+          ],
+          methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+          headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+          credentials: true,
+          maxAge: 3600
       },
-    },
-    'strapi::poweredBy',
-    'strapi::logger',
-    'strapi::query',
-    'strapi::body',
-    'strapi::favicon',
-    'strapi::public',
-  ];
-  
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::favicon',
+  'strapi::public',
+];
