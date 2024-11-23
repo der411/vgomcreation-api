15bfc4075b9262e58525f6cd565006014b61dd39
der411 <vaitilingomvincent@gmail.com>
2024-11-23 18:37:34 +0100

    cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,29 +1,36 @@
 module.exports = ({ env }) => [
     'strapi::errors',
     {
-        name: 'strapi::security',
-        config: {
-            contentSecurityPolicy: {
-                useDefaults: true,
-                directives: {
-                    'connect-src': ["'self'", 'https:'],
-                    'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-                    'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-                    upgradeInsecureRequests: null,
-                },
-            },
-            cors: {
-                origin: ['http://localhost:3000', 'https://vgomcreation-fullstack.vercel.app'],
-                methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-                headers: ['Content-Type', 'Authorization'],
-            },
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+            'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+            upgradeInsecureRequests: null,
+          },
         },
+      },
     },
-    'strapi::cors', // Middleware CORS
+    {
+      name: 'strapi::cors',
+      config: {
+        enabled: true,
+        origin: [
+          'http://localhost:3000', // Frontend local
+          'https://vgomcreation-fullstack.vercel.app', // Frontend déployé
+        ],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        headers: ['Content-Type', 'Authorization'],
+      },
+    },
     'strapi::poweredBy',
     'strapi::logger',
     'strapi::query',
     'strapi::body',
     'strapi::favicon',
     'strapi::public',
-];
+  ];
+  
