a24668009f50f0a1ef6c28d00fbca534e3ec97a1
der411 <vaitilingomvincent@gmail.com>
2024-11-24 13:01:25 +0100

    fichier server

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,8 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('URL', 'http://localhost:1337'),
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),  // Très important : spécifie l'écoute sur toutes les interfaces
+  port: env.int('PORT', 1337),   // Utilise la variable PORT de Railway
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('PUBLIC_URL', 'https://vgomcreation-api-production.up.railway.app'),
 });
