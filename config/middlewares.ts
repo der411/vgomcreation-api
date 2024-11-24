676c22c3ac790c5050a44f83265b64bb33042907
der411 <vaitilingomvincent@gmail.com>
2024-11-24 13:24:40 +0100

    fichier middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,34 +1,38 @@
 module.exports = ({ env }) => [
-    'strapi::errors',
-    {
-        name: 'strapi::security',
-        config: {
-            contentSecurityPolicy: {
-                useDefaults: true,
-                directives: {
-                    'connect-src': ["'self'", 'https:', 'http:'],
-                    'img-src': ["'self'", 'data:', 'blob:', '*'],
-                    'media-src': ["'self'", 'data:', 'blob:', '*'],
-                    upgradeInsecureRequests: null,
-                },
-            },
-        },
-    },
-    {
-        name: 'strapi::cors',
-        config: {
-            enabled: true,
-            headers: ['*'],  // Autorise tous les headers
-            origin: ['*'],   // Autorise toutes les origines pendant le dev
-            methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-            credentials: true,
-            maxAge: 86400,
-        },
-    },
-    'strapi::poweredBy',
-    'strapi::logger',
-    'strapi::query',
-    'strapi::body',
-    'strapi::favicon',
-    'strapi::public',
+  'strapi::errors',
+  {
+      name: 'strapi::security',
+      config: {
+          contentSecurityPolicy: {
+              useDefaults: true,
+              directives: {
+                  'connect-src': ["'self'", 'https:'],
+                  'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+                  'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+                  upgradeInsecureRequests: null,
+              },
+          },
+      },
+  },
+  {
+      name: 'strapi::cors',
+      config: {
+          enabled: true,
+          origin: [
+              'http://localhost:3000', // Frontend local
+              'https://vgomcreation-fullstack.vercel.app', // Frontend déployé
+              'https://vgomcreation-api-production.up.railway.app', // Backend déployé
+          ],
+          methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+          headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+          credentials: true,
+          maxAge: 3600
+      },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::favicon',
+  'strapi::public',
 ];
