500bbd21e76f16003323f3a32f40e2167bd83959
der411 <vaitilingomvincent@gmail.com>
2024-11-28 15:08:11 +0100

    configuration plugin  pour stockage des images

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'local',
+        providerOptions: {
+          sizeLimit: 10000000, // Limite de taille en octets (10 Mo ici)
+        },
+        actionOptions: {
+          upload: {
+            path: env('UPLOAD_PATH', '/data/uploads'),
+          },
+        },
+      },
+    },
+  });
+  
