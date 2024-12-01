758c8f1f66f102adad011dfe1cb7f5085eceeeb7
der411 <vaitilingomvincent@gmail.com>
2024-12-01 14:03:14 +0100

    config mmiddleware & plugin cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,13 +1,17 @@
-export default ({ env }: { env: (key: string) => string }) => ({
+export default ({ env }) => ({
     upload: {
-        provider: 'cloudinary',
-        providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET'),
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
         },
     },
 });
-
-
-  
