cab1fa506cbd07612576a5ff039076483f93caa0
der411 <vaitilingomvincent@gmail.com>
2024-12-01 09:43:29 +0100

    correction controller paiment

--- src/api/payment/controllers/payment.ts
+++ src/api/payment/controllers/payment.ts
@@ -1,20 +1,26 @@
-
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = {
     async createPaymentIntent(ctx) {
         const { amount, currency } = ctx.request.body;
 
+        if (!amount || !currency) {
+            ctx.badRequest('Le montant ou la devise est manquant.');
+            return;
+        }
+
         try {
             const paymentIntent = await stripe.paymentIntents.create({
-                amount: amount * 100, // Convertir en centimes
+                amount, // Le montant doit être en centimes
                 currency,
-                payment_method_types: ['card'],
+                payment_method_types: ['card'], // Méthodes de paiement autorisées
             });
 
             ctx.send({ clientSecret: paymentIntent.client_secret });
-        } catch (err) {
-            ctx.badRequest('Erreur lors de la création du Payment Intent', { error: err.message });
+        } catch (error) {
+            console.error('Erreur Stripe :', error.message);
+            ctx.badRequest('Erreur lors de la création du Payment Intent.', { error: error.message });
         }
     },
 };
+
