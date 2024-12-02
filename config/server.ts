2738f8958ba257a4f570442ec918420ac677d2b2
der411 <vaitilingomvincent@gmail.com>
2024-12-02 09:33:11 +0100

    suppression payment

--- config/server.ts
+++ config/server.ts
@@ -12,7 +12,10 @@ module.exports = ({ env }) => ({
   },
   settings: {
     cors: {
-      origin: ['https://www.vgomcreation.fr', 'https://vgomcreation.fr', 'http://localhost:3000'],
+      origin: ['https://www.vgomcreation.fr', 'https://vgomcreation.fr', 'http://localhost:3000'], // Définir les origines autorisées pour le CORS
+    },
+    public: {
+      path: './public', // Répertoire pour servir les fichiers publics
     },
   },
 });
