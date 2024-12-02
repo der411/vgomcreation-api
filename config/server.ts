fa0828d5c9ef66c447e416cf1aabd7c5500b7cff
der411 <vaitilingomvincent@gmail.com>
2024-12-02 09:10:16 +0100

    url prod

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'https://api.vgomcreation.fr'),
+  url: env('URL', 'https://vgomcreation-api.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
