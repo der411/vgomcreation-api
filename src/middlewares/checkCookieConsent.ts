e39895b1f143ecb01de7ec59f732b1ad7d9f4126
dokapax <dokapax957@rustetic.com>
2024-12-21 03:59:00 +0100

    function checkCookieConsent

--- src/middlewares/checkCookieConsent.ts
+++ src/middlewares/checkCookieConsent.ts
@@ -1,19 +1,19 @@
 import { Context, Next } from 'koa';
 
-const checkCookieConsent = async (ctx: Context, next: Next): Promise<void> => {
-    const cookieConsent = ctx.cookies.get('cookieConsent'); // Lire le cookie "cookieConsent"
+export default (config: Record<string, unknown>, { strapi }: { strapi: any }) => {
+    return async (ctx: Context, next: Next): Promise<void> => {
+        const cookieConsent = ctx.cookies.get('cookieConsent');
 
-    if (cookieConsent === 'true' || ctx.path.startsWith('/public')) {
-        // Autoriser l'accès si le consentement est donné ou si la route est publique
-        await next();
-    } else if (ctx.path === '/api/cookie-consent') {
-        // Autoriser l'accès à la route spécifique de gestion des cookies
-        await next();
-    } else {
-        // Bloquer l'accès si aucune autorisation n'a été donnée
-        ctx.status = 403;
-        ctx.body = { message: 'Consentement aux cookies requis' };
-    }
+        if (cookieConsent === 'true' || ctx.path.startsWith('/public')) {
+            // Consentement accepté ou accès à une ressource publique
+            await next();
+        } else if (ctx.path === '/api/cookie-consent') {
+            // Autoriser l'accès à la route spécifique de gestion des cookies
+            await next();
+        } else {
+            // Bloquer l'accès si aucune autorisation n'a été donnée
+            ctx.status = 403;
+            ctx.body = { message: 'Consentement aux cookies requis' };
+        }
+    };
 };
-
-export default checkCookieConsent;
