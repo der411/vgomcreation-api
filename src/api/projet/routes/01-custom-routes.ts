fe220b0bb5b782d8d1407d1b5cd510e812bdd7dd
ritik71603 <ritik71603@xcmexico.com>
2025-01-19 12:37:12 +0100

    routes custom projet

---
+++ src/api/projet/routes/01-custom-routes.ts
@@ -0,0 +1,30 @@
+// src/api/projet/routes/01-custom-routes.ts
+export default {
+    routes: [
+        {
+            method: 'POST',
+            path: '/projets/create-checkout-session',
+            handler: 'api::projet.projet.createCheckoutSession',
+            config: {
+                auth: false,
+            }
+        },
+        {
+            method: 'POST',
+            path: '/projets/webhook',
+            handler: 'api::projet.projet.handleWebhook',
+            config: {
+                auth: false,
+                middlewares: []
+            }
+        },
+        {
+            method: 'GET',
+            path: '/projets/retrieve-session/:sessionId',
+            handler: 'api::projet.projet.retrieveSession',
+            config: {
+                auth: false,
+            }
+        }
+    ]
+};
