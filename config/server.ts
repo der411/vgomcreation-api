898b82b56807e1d4a27619d0280bc171206b2348
ritik71603 <ritik71603@xcmexico.com>
2025-01-22 13:39:27 +0100

    ajout de l'url client dans server strapi

--- config/server.ts
+++ config/server.ts
@@ -1,13 +1,16 @@
 module.exports = ({ env }) => {
   const url = env('URL', 'https://vgomcreation-api.onrender.com');
+  const client_url = env('CLIENT_URL', 'https://www.vgomcreation.fr');
 
-  // Log pour vérifier la valeur de l'URL
-  console.log('URL utilisée par Strapi :', url);
+  // Logs pour vérifier les valeurs de configuration
+  console.log('URL utilisée par Strapi (Backend) :', url);
+  console.log('CLIENT_URL utilisée pour le Frontend :', client_url);
 
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     url, // Utilisation de la constante url
+    client_url, // Ajout explicite de la constante client_url pour l'utiliser dans les templates ou autres
     app: {
       keys: env.array('APP_KEYS'),
     },
@@ -18,7 +21,11 @@ module.exports = ({ env }) => {
     },
     settings: {
       cors: {
-        origin: ['https://www.vgomcreation.fr', 'https://vgomcreation.fr', 'http://localhost:3000'], // Origines autorisées pour le CORS
+        origin: [
+          'https://www.vgomcreation.fr',
+          'https://vgomcreation.fr',
+          'http://localhost:3000',
+        ], // Origines autorisées pour le CORS
       },
       public: {
         path: './public', // Répertoire pour servir les fichiers publics
