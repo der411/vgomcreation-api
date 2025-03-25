5fc5fb7edf9710af65df7241998888488da9c96d
ritik71603 <ritik71603@xcmexico.com>
2025-03-25 09:46:56 +0100

    force https cookies

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -14,12 +14,17 @@ module.exports = ({ env }) => [
             contentSecurityPolicy: {
                 useDefaults: true,
                 directives: {
-                    'connect-src': ["'self'", "https://accounts.google.com"],
+                    'connect-src': ["'self'", "https://accounts.google.com", 'https:'],
                     'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
                     'frame-src': ["'self'", "https://accounts.google.com"],
                     'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
                     upgradeInsecureRequests: null,
                 },
+            },
+            cookies: {
+                secure: true, // Force HTTPS
+                httpOnly: true, // Sécurité supplémentaire
+                sameSite: 'strict', // Politique de même site
             },
             forceHttps: true,
             trustProxy: true,
