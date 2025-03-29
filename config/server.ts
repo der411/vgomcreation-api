88ebb8d79eb8894a12c3667bd6e13291f1f195bc
ritik71603 <ritik71603@xcmexico.com>
2025-03-29 09:27:54 +0100

    config/server restructuré

--- config/server.ts
+++ config/server.ts
@@ -1,20 +1,9 @@
-module.exports = ({ env }) => {
-  const url = env('URL', 'https://vgomcreation-api.onrender.com');
-  const client_url = env('CLIENT_URL', 'https://www.vgomcreation.fr');
-
-  return {
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    proxy: env.bool('PROXY', true),
-    url,
-    client_url,
-    app: {
-      keys: env.array('APP_KEYS'),
-    },
-    admin: {
-      auth: {
-        secret: env('ADMIN_JWT_SECRET'),
-      },
-    },
-  };
-};
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('URL', 'https://vgomcreation-api.onrender.com'),
+  proxy: env.bool('PROXY', true),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
